9b84a5c6992121435d87315702a1be3592de9663
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-09-24 15:11:07 +0700

    đẩy lên bài tập thực hành Lesson2_timeviewingapplication

---
+++ lesson2_SpringController/TimeViewingApplication/src/main/java/vn/codegym/timeviewingapplication/AppInit.java
@@ -0,0 +1,21 @@
+package vn.codegym.timeviewingapplication;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {//nơi khai báo beans dành cho service/data//....
+        return new Class<?>[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {//nơi khai báo beans dành cho web layer(controller,view,...)
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {// đường dẫn gốc mappping"/"
+        return new String[]{"/"};
+    }
+}
---
+++ lesson2_SpringController/TimeViewingApplication/src/main/java/vn/codegym/timeviewingapplication/WebConfig.java
@@ -0,0 +1,27 @@
+package vn.codegym.timeviewingapplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "vn.codegym.timeviewingapplication.controllers")//load beans --> controllers là các object(beans)
+public class WebConfig implements WebMvcConfigurer {
+
+    // beans dành cho view resolver
+    @Bean
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        // đường dẫn đến view ở đâu
+        viewResolver.setPrefix("/WEB-INF/");
+        // đuôi file view lad gì (jsp)
+        viewResolver.setSuffix(".jsp");
+        // font hiển thị tiếng việt
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        return viewResolver;
+    }
+}
