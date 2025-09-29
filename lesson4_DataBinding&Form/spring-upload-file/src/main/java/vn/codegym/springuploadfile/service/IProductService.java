175dc06fd21757c538189646474a6e965a5c8059
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-09-29 16:43:50 +0700

    đẩy lên bài tập Lesson4_[Thực hành] Upload file

---
+++ lesson4_DataBinding&Form/spring-upload-file/src/main/java/vn/codegym/springuploadfile/service/IProductService.java
@@ -0,0 +1,17 @@
+package vn.codegym.springuploadfile.service;
+
+import vn.codegym.springuploadfile.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+}
