26b59002291a16a8f9a786dbfd89fe9e884091a2
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-10-19 13:59:10 +0700

    đẩy lên bài tập thực hành lesson6_[Thực hành] Ứng dụng quản lý danh sách khách hàng (v.2025)

---
+++ lesson6_JPA/democustomer-management-jpa/src/main/java/vn/codegym/democustomermanagementjpa/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package vn.codegym.democustomermanagementjpa.repository;
+
+import vn.codegym.democustomermanagementjpa.model.Customer;
+
+public interface ICustomerRepository extends IGenerateRepository<Customer>{
+}
---
+++ lesson6_JPA/democustomer-management-jpa/src/main/java/vn/codegym/democustomermanagementjpa/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package vn.codegym.democustomermanagementjpa.service;
+
+import vn.codegym.democustomermanagementjpa.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer> {
+}
