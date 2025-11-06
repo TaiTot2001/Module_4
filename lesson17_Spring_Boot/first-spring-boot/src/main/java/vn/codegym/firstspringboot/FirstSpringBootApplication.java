25700d0b0007ba32ec91f7456c9db8ed4412aef3
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-11-06 16:20:21 +0700

    đẩy lên bài tập lesson17_[Thực hành] Tạo ứng dụng web đơn giản với Spring Boot (v.2025)

---
+++ lesson17_Spring_Boot/first-spring-boot/src/main/java/vn/codegym/firstspringboot/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package vn.codegym.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootApplication.class, args);
+    }
+
+}
