c467ec6e78d199de852b9422ba397945596422db
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-11-06 16:18:06 +0700

    đẩy lên bài tập lesson18_[Thực hành] Spring Boot JSON Web Token (JWT) (v.2025)

---
+++ lesson18_Spring_Boot_Starters_&_Common_Configurations/spring-boot-security-jwt/src/main/java/vn/codegym/springbootsecurityjwt/configuration/SecurityInit.java
@@ -0,0 +1,6 @@
+package vn.codegym.springbootsecurityjwt.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
