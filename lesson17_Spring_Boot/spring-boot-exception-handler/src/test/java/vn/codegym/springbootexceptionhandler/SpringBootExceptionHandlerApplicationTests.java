dbc21baf97fcbfcd78ec69f83aea20ea724de1a4
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-11-06 16:36:07 +0700

    đẩy lên bài tập lesson17_[Thực hành] Spring Boot xử lý Exception (v.2025)

---
+++ lesson17_Spring_Boot/spring-boot-exception-handler/src/test/java/vn/codegym/springbootexceptionhandler/SpringBootExceptionHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.codegym.springbootexceptionhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
