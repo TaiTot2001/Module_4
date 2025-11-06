454544d2c0385b761a68d175818e140dcae7b0c3
TaiTot2001 <nguyentrongtaihong@gmail.com>
2025-11-06 17:24:29 +0700

    đẩy lên bài tập lesson17_[Thực hành] Ứng dụng quản lý khách hàng sử dụng Spring Boot (v.2025)

---
+++ lesson17_Spring_Boot/spring-boot-customer-management/src/main/java/vn/codegym/springbootcustomermanagement/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package vn.codegym.springbootcustomermanagement.repository;
+
+import vn.codegym.springbootcustomermanagement.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ lesson17_Spring_Boot/spring-boot-customer-management/src/main/java/vn/codegym/springbootcustomermanagement/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package vn.codegym.springbootcustomermanagement.service;
+
+import vn.codegym.springbootcustomermanagement.model.Customer;
+
+public interface ICustomerService extends IGenerateService<Customer> {
+}
